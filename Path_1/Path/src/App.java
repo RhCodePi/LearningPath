043d4c308b5151c26f8a7352bb28003311925832
Abdullah <abdullahv2730@gmail.com>
2022-03-06 15:41:56 +0300

    Update App.java
    Add getter and setter

--- Path_1/Path/src/App.java
+++ Path_1/Path/src/App.java
@@ -1,22 +1,51 @@
 
 class Person {
     // This is class but we use like a structure.
-    String name;
-    String surname;
-    byte old;
+    private String name;
+    private String surname;
+    private byte old;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void SetSurname(String surname)
+    {
+        this.surname = surname;
+    }
+
+    public void setOld(Byte old)
+    {
+        this.old = old;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public String getSurname()
+    {
+        return surname;
+    }
+    public Byte getOld()
+    {
+        return old;
+    }
+
+
 }
 
 
 public class App {
     public static void main(String[] args) throws Exception {
         // initial the Person class
-        Person newPerson;
-        newPerson = new Person();
-        newPerson.name = "Abdullah";
-        newPerson.surname = "Vural";
-        newPerson.old = 20;
+        Person newPerson = new Person();
+        newPerson.setName("Abdullah");
+        newPerson.SetSurname("Vural");
+        newPerson.setOld((byte) 20);
 
-        System.out.println(newPerson.name + "  " + newPerson.surname + "   " + newPerson.old);
+        System.out.println(newPerson.getName() + "  " + newPerson.getSurname() + "   " + newPerson.getOld());
 
 
     }
